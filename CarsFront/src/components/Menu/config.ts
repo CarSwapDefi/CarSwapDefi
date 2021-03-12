a6cda700b871f33b3a82ad1529532fc50dea57c9
ATC9 <79402847+ATC9@users.noreply.github.com>
2021-03-12 14:32:15 -0500

    updated telegram/twitter links

---
+++ CarsFront/src/components/Menu/config.ts
@@ -0,0 +1,90 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://exchange.pancakeswap.finance/#/swap?inputCurrency=0xFbe822E7258E276F14cB87ac6e2b0Bf611c29302',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.pancakeswap.finance/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'AutoShow',
+    icon: 'PoolIcon',
+    href: '/nests',
+  },
+  
+//  {
+//    label: 'Info',
+//    icon: 'InfoIcon',
+//    items: [
+  //    {
+  //      label: 'PancakeSwap',
+  //      href: 'https://pancakeswap.info/token/0x7c5BefeDe8b361283DA678aCbDA70Ba2d96e2D44',
+  //    },
+  //    
+  //    {
+  //      label: 'CoinMarketCap',
+  //      href: 'https://coinmarketcap.com/currencies/car-finance/',
+  //    },
+  //    {
+  //      label: 'CoinGecko',
+  //      href: 'https://www.coingecko.com/en/coins/car-finance',
+  //    },
+//    ],
+//  },
+  {
+    label: 'Twitter',
+    icon: 'InfoIcon',
+    href: 'https://twitter.com/CarSwapDefi',
+  },
+  {
+    label: 'Telegram',
+    icon: 'InfoIcon',
+    href: 'https://t.me/carswap',
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/CarSwapDefi/CarSwapDefi',
+      },
+      {
+        label: 'Docs',
+        href: 'https://carswapdefi.gitbook.io/car-swap-defi/',
+      },
+//      {
+//        label: 'Blog',
+//        href: 'https://goosefinance.medium.com/',
+//      },
+    ],
+  },
+  
+//  {
+//    label: 'Audit by Hacken',
+//    icon: 'AuditIcon',
+//    href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
+//  },
+  
+]
+
+export default config
---
+++ CarsFront/src/style/Global.tsx
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components'
+// eslint-disable-next-line import/no-unresolved
+import { PancakeTheme } from '@pancakeswap-libs/uikit'
+
+declare module 'styled-components' {
+  /* eslint-disable @typescript-eslint/no-empty-interface */
+  export interface DefaultTheme extends PancakeTheme {}
+}
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Kanit', sans-serif;
+  }
+  .sc-eCssSg >.kEyEBW{    display: none!important  }
+  .fvYuMC{    
+    display: none!important  
+    }
+  body {
+    background-color: ${({ theme }) => theme.colors.background};
+
+    img {
+      height: auto;
+      max-width: 100%;
+    }
+  }
+`
+
+export default GlobalStyle
